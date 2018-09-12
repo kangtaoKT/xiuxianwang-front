3c532b1b67f31ee4dfa9652aa4fe344043e00004
kangtao <bbdkangtao>
2018-09-12 17:29:17 +0800

    滚动表格

--- app/containers/chuRuXianMen/index.js
+++ app/containers/chuRuXianMen/index.js
@@ -11,6 +11,14 @@ export default class ChuRuXianMen extends Component {
         {label: '请选择', value: '请选择'},
         {label: 'one', value: 'one'},
         {label: 'two', value: 'two'}
+      ],
+      title: [
+        'title1', 'title2', 'title3', 'title4', 'title5', 'title6',
+        'title7', 'title8', 'title9', 'title10'
+      ],
+      data: [
+        'data1', 'data2', 'data3', 'data4', 'data5', 'data6', 'data7',
+        'data8', 'data9', 'data10', 'data11', 'data12'
       ]
     }
   }
@@ -21,7 +29,16 @@ export default class ChuRuXianMen extends Component {
       [name]: label
     })
   };
+  scrollChange = () => {
+    let ktScroll = document.getElementById('ktScroll');
+    let ktTitle = document.getElementById('ktTitle');
+    let scrollHeight = ktScroll? ktScroll.scrollLeft: 0;
+    if(ktTitle) {
+      ktTitle.style.left = `-${scrollHeight}px`;
+    }
+  };
   render() {
+    let { title, data } = this.state;
     return (
       <div style={{padding: '50px'}}>
         <MySelect
@@ -30,6 +47,45 @@ export default class ChuRuXianMen extends Component {
           handleClick={this.handleClick}
           name="showText"
         />
+        <div className={style['container']}>
+          <div  className={style['title']}>
+            <div id='ktTitle' className={style['scroll-title']}>
+              <ul>
+                <li>
+                  {
+                    title.map((obj, index) => (
+                      <span key={index}>{obj}</span>
+                    ))
+                  }
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div onScroll={this.scrollChange} id='ktScroll' className={style['body']}>
+            <ul>
+              <li>
+                {
+                  title.map((obj, index) => (
+                    <span key={index}>{obj}</span>
+                  ))
+                }
+              </li>
+            </ul>
+            <ul>
+              {
+                data.map((obj, index) => (
+                  <li key={index}>
+                    {
+                      title.map((ob, i) => (
+                        <span key={i}>{ob}</span>
+                      ))
+                    }
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
